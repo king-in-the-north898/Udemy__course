fa6dd3c465c44c301bfd163af42d42e6729fb66b
Danil <danyakutsenko@gmail.com>
2020-08-05 14:11:17 +0300

    All commited

---
+++ app.js
@@ -0,0 +1,27 @@
+"use strict"
+alert("gflg");
+
+//confirm
+const result = confirm("Are you Ok?")
+console.log(result);
+
+
+
+// interpolation
+
+const master = "VAN";
+console.log(`http/ff/"${master}/2`);
+const user = "Ivan";
+
+// operators
+
+const isChecked = true;
+isClosed = true;
+
+console.log(isChecked && isClosed);
+
+
+
+
+
+
